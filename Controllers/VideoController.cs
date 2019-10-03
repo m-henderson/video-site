1e6e024659b1b3b1fd5e226c44b2ed99e15741fb
michael.v.henderson@icloud.com <michael@Michaels-iMac.local>
2019-10-03 06:54:44 -0500

    create get method for video

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using video_site.Models;
 
 namespace video_site.Controllers {
+    
+    [Route("api/[controller]")]
     public class VideoController : Controller {
 
+        [HttpGet("[action]")]
+        public IEnumerable<Video> Get() {
+            var videoList = new List<Video>();
+            return videoList;
+        }
     }
 }
