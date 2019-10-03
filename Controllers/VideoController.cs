f6681d21cde0253455ed4e0157f2141eda1afe65
michael.v.henderson@icloud.com <michael@Michaels-iMac.local>
2019-10-03 06:48:31 -0500

    create video controller and class on api

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace video_site.Controllers {
+    public class VideoController : Controller {
+
+    }
+}
---
+++ Models/Video.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace video_site.Models {
+    // this class is used to model a video object
+    public class Video {
+        // primary key of video
+        public int Id { get; set; }
+        // date entity was created // TODO: move to entity base
+        public DateTime Created { get; set; }
+        // date entity was updated // TODO: move to entity base
+        public DateTime Updated { get; set; }
+        // The title of the video
+        public string Title { get; set; }
+        
+        // The detailed description of the video
+        public string Description { get; set; }
+
+        // ASP.NET Core actually has a way of handling
+        // files with a new type. we can set that type later
+        // The actual file object of the video being uploaded
+        public string File { get; set; }
+
+        // The tags used for curating and sorting via algorythm
+        public ICollection<string> Tags { get; set; }
+
+        // The playlist category that the video is associated with
+        public int PlayListId { get; set; }
+
+        // Bit value to determine if the user wants the video to be 
+        // currated behind the metered paywall
+        public bool BehindPayWall { get; set; }
+    }
+}
